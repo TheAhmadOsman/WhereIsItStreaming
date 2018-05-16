94d1faca08183599129e3c4be3f07b6fcd4fa4cd
Ahmad Osman <ahmad.osman@luther.edu>
2018-05-16 16:38:23 -0500

    Structure

--- parse.py
+++ parse.py
@@ -1,8 +1,7 @@
 import csv
 import sqlite3
-import imdb
-#fetch the csv files from https://www.kaggle.com/rounakbanik/the-movies-dataset
-#drop in a folder called kaggle and run, it should take less than 5 seconds
+# fetch the csv files from https://www.kaggle.com/rounakbanik/the-movies-dataset
+# drop in a folder called kaggle and run, it should take less than 5 seconds
 
 db = sqlite3.connect("movies.db")
 curs = db.cursor()
@@ -13,13 +12,15 @@ stars = open("kaggle/credits.csv", "r")
 movieReader = csv.DictReader(csvfile)
 starReader = csv.DictReader(stars)
 for row in movieReader:
-    curs.execute('''INSERT INTO movies(imdb_id, id, genres, title, release_date, homepage, poster_path, tagline) VALUES(?,?,?,?,?,?,?,?)''', (row["imdb_id"], row["id"], row["genres"], row["title"], row["release_date"], row["homepage"], row["poster_path"], row["tagline"]))
+    curs.execute('''INSERT INTO movies(imdb_id, id, genres, title, release_date, homepage, poster_path, tagline) VALUES(?,?,?,?,?,?,?,?)''',
+                 (row["imdb_id"], row["id"], row["genres"], row["title"], row["release_date"], row["homepage"], row["poster_path"], row["tagline"]))
 
 curs.execute("DROP TABLE IF EXISTS casts")
 curs.execute("CREATE TABLE IF NOT EXISTS casts(id TEXT, cast TEXT, crew TEXT)")
 for row in starReader:
-    curs.execute('''INSERT INTO casts(id, cast, crew) VALUES(?,?,?)''', (row["id"], row["cast"], row["crew"]))
-    
+    curs.execute('''INSERT INTO casts(id, cast, crew) VALUES(?,?,?)''',
+                 (row["id"], row["cast"], row["crew"]))
+
 csvfile.close()
 stars.close()
 
